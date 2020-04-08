2e0ab944311269c4aee71b67192e81de7e64650f
anthonyminyungi <dbstnsdl12@naver.com>
2020-04-08 20:25:22 +0900

    done of counter practice using redux

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -4,11 +4,39 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
+number.innerText = 0;
+
+const ADD = 'ADD';
+const MINUS = 'MINUS';
+
 // reducer:: function returns data in your application (only it could modify state)
-const countModifier = (count = 0) => {
+const countModifier = (count = 0, action) => {
   // make default state initially
-  console.log(count);
-  return count;
-};
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
+}; // reducer returns "current" State
 
 const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD }); // dispatch sends object that describes action to reducer, it must have the field "type"
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
