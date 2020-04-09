ac1cb5a1ef7aefdb0c7fed6b73f9158c14fee03c
anthonyminyungi <dbstnsdl12@naver.com>
2020-04-09 11:25:03 +0900

    #4.1 createAction in redux toolkit

--- react-redux-todo/src/store.js
+++ react-redux-todo/src/store.js
@@ -1,29 +1,16 @@
 import { createStore } from 'redux';
+import { createAction } from '@reduxjs/toolkit';
 
-const ADD = 'ADD';
-const DELETE = 'DELETE';
-
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
+const addToDo = createAction('ADD'); // composed of type, payload (it is an convention)
+const deleteToDo = createAction('DELETE');
 
 // enhancement : use local storage to save state on the browser
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
